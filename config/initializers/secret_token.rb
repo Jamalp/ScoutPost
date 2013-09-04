b6481e3ecec82c3ee89d1e5d495b6c7254e4da57
Jamal Powell <jamalkpowell@gmail.com>
2013-09-04 11:08:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScoutPost::Application.config.secret_key_base = '691385d78f527df212ad114b832a6b6c69f31e32430ece4b20b15e3fd223e384a3a809f6a8bccd93719f93fa8d17ea68bc1d27777411363ab3899275e37c36ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScoutPost::Application.config.session_store :cookie_store, key: '_ScoutPost_session'
