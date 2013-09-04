bbaa7aac1bf70974fe4de903fd6ee85561f6d1f5
Wesley Carr <surfswell17@gmail.com>
2013-09-04 13:40:26 -0400

    routes setup for devise modules, main controller and subscriber digest

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ScoutPost::Application.routes.draw do
   devise_for :subscribers
   root :to => "main#home"
   get '/about' => 'main#about'
+  get '/subscriber/digest' => 'subscriber#digest'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
