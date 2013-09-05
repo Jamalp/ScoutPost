4689d2142d11c26943df2c08683d7d2b2d780a43
Jamal Powell <jamalkpowell@gmail.com>
2013-09-05 11:07:50 -0400

    added time column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130904183836) do
+ActiveRecord::Schema.define(version: 20130904212514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20130904183836) do
     t.integer  "hn_post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "submission"
   end
 
   add_index "posts", ["created_at"], name: "index_posts_on_created_at", using: :btree
