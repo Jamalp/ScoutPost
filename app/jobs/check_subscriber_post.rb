38c172a266731cb00ffd77acf1901e7989cdd15a
Beryl <beryl.schragger@gmail.com>
2013-09-12 15:03:40 -0400

    Added favorites to navbar, and added checksubscriberpost, which creates subscriber-post connections.

--- app/jobs/check_subscriber_post.rb
+++ app/jobs/check_subscriber_post.rb
@@ -3,18 +3,32 @@ class CheckSubscriberPost
 
   def self.perform(post_id)
     post = Post.find(post_id)
-    id = current_subscriber.id
-    # if post.
-    favorites = Favorite.all
-    favorites.each do |favorite|
-      if current_subscriber.id == favorite.subscriber_id && post.score >= favorite.score_threshold
-        puts 'hi'
-        # Resque.enqueue(SendSms, phonenumber, text)
-      else
-        puts "Construct additional pylons"
+    subscribers = Subscriber.all
+    subscribers.each do |subscriber|
+      begin
+        found_post = subscriber.posts.find(post_id)
+      rescue ActiveRecord::RecordNotFound
+        found_post = nil
+      end
+      if found_post.nil?
+        favorites = Favorite.where(subscriber_id: subscriber.id)
+        added = false
+        favorites.each do |favorite|
+          puts favorite.tag.name
+          tag_id = favorite.tag.id
+          # if post is tagged with a subscriber's favorited tag
+          begin
+            if post.tags.find(tag_id) && post.score >= favorite.score_threshold
+              puts "#{favorite.tag.name}: found this one"
+              # Resque.enqueue(SendSms, subscriber.phone, "We found a new article about #{favorite.tag.name}!")
+              # Resque.enqueue(SendEmail, whatever else needs to go here)
+              subscriber.posts << post
+            end
+          rescue ActiveRecord::RecordNotFound
+          end
+        end
       end
     end
   end
-end
 
-#
+end
